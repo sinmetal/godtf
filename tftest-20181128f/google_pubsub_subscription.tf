d68586c070897e9ad20720dce10dbdc1d4d5df78
sinmetal <metal.tie@gmail.com>
2018-12-08 19:08:32 +0900

    set project refs #3

--- tftest-20181128f/google_pubsub_subscription.tf
+++ tftest-20181128f/google_pubsub_subscription.tf
@@ -1,4 +1,5 @@
 resource "google_pubsub_subscription" "d-mytopic-pull" {
-  name  = "d-mytopic-pull"
-  topic = "projects/tftest-20181128d/topics/mytopic"
+  project = "tftest-20181128f"
+  name    = "d-mytopic-pull"
+  topic   = "projects/tftest-20181128d/topics/mytopic"
 }
