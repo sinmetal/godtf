b1f469d11587e8dafdb1f24651731c76d4455682
sinmetal <metal.tie@gmail.com>
2019-02-12 16:31:58 +0900

    いったん ip addr 指定消してみた

--- tftest-20181128f/google_compute_instance.tf
+++ tftest-20181128f/google_compute_instance.tf
@@ -18,9 +18,7 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
 
-    access_config {
-      nat_ip = "{$google_compute_address.my_address.address}"
-    }
+    access_config {}
   }
 
   metadata {
